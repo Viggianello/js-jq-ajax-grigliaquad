56b6ae467d7c701c6132e3a601cf179077e96155
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-05-19 16:30:10 +0200

    esercizio con bonus

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // var d = new Date();
 // var orario = d.getHours() + ':' + d.getMinutes();
+
+
 // Create in HTML una griglia di 36 quadratini (6x6).
 // Ad ogni click su un quadratino, parte una richiesta ajax per recuperare un numero random tra 1 e 9.
 // Se il numero restituito dall'api è <= 5, il quadrato su cui l'utente ha cliccato diventa giallo; se invece il numero restituito dall'api è > 5, il quadrato su cui l'utente ha cliccato diventa verde.
@@ -14,41 +16,44 @@
 var quadratinigriglia = $('#entry-template').html();
 var template_function = Handlebars.compile(quadratinigriglia);
 for (var i = 0; i < 36; i++) {
-    var ilMioNuovoquadratino = {
-        // // cambio il suo testo
-        // numero: ,
-        // // aggiungo la classe
-        // classe: 'inviato'
-    };
-    var html_finale = template_function(ilMioNuovoquadratino);
+    var html_finale = template_function();
     $('.griglia').append(html_finale);
 }
 
 // Associo ad ogni click del quadratino
-
-// richiamo ajax per recuperare un numero random tra 1 e 9
-// $.ajax({
-//     'url': 'https://flynn.boolean.careers/exercises/api/random/int',
-//     'method': 'GET',
-//     'success': function(numero) {
-//         var numerorandom = numero.responce;
-//         numcolor(numerorandom)
-//     },
-//     'error': function () {
-//         alert('non va nulla');
-//     }
-// })
+$(document).on('click','.quadratino',  function() {
+    // recupero il quadrato corrente
+    var quadratocorrente = $(this);
+    // richiamo ajax per recuperare un numero random tra 1 e 9
+    // questo è una funzione
+    $.ajax({
+        // qui parte l'oggetto
+        'url': 'https://flynn.boolean.careers/exercises/api/random/int',
+        // risponde con un numero random da 1 a 9
+        'method': 'GET',
+        'success': function(numero) {
+            // console.log(numero);
+            var numerorandom = numero.response;
+            numcolor(numerorandom, quadratocorrente)
+        }
+    }// fine oggetto
+    );
+}// chiusura funzione del click
+);// chiusura click
 
 // creo una funzione per associare il numero random hai colori
-// function numcolor(numero) {
-//     // Se il numero (restituito dall'api) è <= 5,
-//     if (numero<= 5) {
-//         // il quadrato su cui l'utente ha cliccato diventa giallo
-//         $('.quadratino').addClas('giallo')
-//     }
-//     // allora il numero (restituito dall'api) è > 5
-//     else {
-//         $('.quadratino').addClas('verde')
-//     }
-//     $('.quadratino').append(numero)
-// }
+function numcolor(numero, quad) {
+    // Se il numero (restituito dall'api) è <= 5,
+    if (numero<= 5) {
+        // il quadrato su cui l'utente ha cliccato diventa giallo
+        quad.addClass('giallo');
+    }
+    // allora il numero (restituito dall'api) è > 5
+    else {
+        quad.addClass('verde');
+        // var oky =$('.quadratino'){
+        //     classe:verde
+        // }
+    }
+    quad.append(numero);
+}
